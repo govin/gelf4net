4cd6459873c0b6e5655e176a9a7d89086890dc1c
Maarten Vanden Branden <firefixmaarten@gmail.com>
2014-02-26 08:54:20 +0100

    Only open connection if not open yet

--- src/Gelf4net/Appender/GelfAmqpAppender.cs
+++ src/Gelf4net/Appender/GelfAmqpAppender.cs
@@ -1,6 +1,7 @@
 ﻿using log4net.Appender;
 using log4net.Util;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Framing.v0_9_1;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -11,6 +12,9 @@ namespace gelf4net.Appender
 {
     public class GelfAmqpAppender : AppenderSkeleton
     {
+        private IConnection connection;
+        private IModel model;
+
         public GelfAmqpAppender()
         {
             Encoding = Encoding.UTF8;
@@ -47,17 +51,49 @@ namespace gelf4net.Appender
 
         protected override void Append(log4net.Core.LoggingEvent loggingEvent)
         {
-            var message = RenderLoggingEvent(loggingEvent).GzipMessage(Encoding);
+            EnsureConnectionIsOpen();
 
-            using (IConnection conn = ConnectionFactory.CreateConnection())
-            {
-                var model = conn.CreateModel();
-                model.ExchangeDeclare("sendExchange", ExchangeType.Direct);
-                model.QueueDeclare(RemoteQueue, true, false, false, null);
-                model.QueueBind(RemoteQueue, "sendExchange", "key");
-                byte[] messageBodyBytes = message;
-                model.BasicPublish("sendExchange", "key", null, messageBodyBytes);
-            }
+            byte[] message = RenderLoggingEvent(loggingEvent).GzipMessage(Encoding);
+            model.BasicPublish("sendExchange", "key", null, message);
+        }
+
+        public void EnsureConnectionIsOpen()
+        {
+            if (model != null) return;
+            OpenConnection();
+        }
+
+        private void OpenConnection()
+        {
+            connection = ConnectionFactory.CreateConnection();
+            connection.ConnectionShutdown += ConnectionShutdown;
+            model = connection.CreateModel();
+
+            model.ExchangeDeclare("sendExchange", ExchangeType.Direct);
+            model.QueueDeclare(RemoteQueue, true, false, false, null);
+            model.QueueBind(RemoteQueue, "sendExchange", "key");
+        }
+
+        void ConnectionShutdown(IConnection shutingDownConnection, ShutdownEventArgs reason)
+        {
+            SafeShutdownForConnection();
+            SafeShutDownForModel();
+        }
+
+        private void SafeShutDownForModel()
+        {
+            if (model == null) return;
+            model.Close(Constants.ReplySuccess, "gelf rabbit appender shutting down!");
+            model.Dispose();
+            model = null;
+        }
+
+        private void SafeShutdownForConnection()
+        {
+            if (connection == null) return;
+            connection.ConnectionShutdown -= ConnectionShutdown;
+            connection.AutoClose = true;
+            connection = null;
         }
     }
 }
